acf125e982163684df32538f59c104a49206967f
mekpro <mekpro@gmail.com>
2011-03-01 17:33:09 +0700

    new

---
+++ android_test/src/wegrass/android/test/home.java
@@ -0,0 +1,13 @@
+package wegrass.android.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class home extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
